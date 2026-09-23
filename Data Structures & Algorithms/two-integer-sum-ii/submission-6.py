c9fc41d9675e9df6ade41da6f4f59d01e72c48da
Artem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2026-09-23 13:10:55 +0200

    Add: two-integer-sum-ii - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        pointer1 = 0
+        pointer2 = len(numbers)-1
+        for i in range(0,len(numbers)):
+            if target < numbers[pointer1] + numbers[pointer2]:
+                pointer2 -=1
+            elif target > numbers[pointer1] + numbers[pointer2]:
+                pointer1 +=1
+            else:
+                break
+
+        return [pointer1+1, pointer2+1]
